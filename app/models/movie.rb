e3bf863d4187170c5866ecd8e1ddb0d38e36de46
Chloe Avenas <avenaschloe@gmail.com>
2021-05-27 12:29:02 +0100

    validations added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+  # if you're not calling the above line, the code movie.bookmarks won't work
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
 end
